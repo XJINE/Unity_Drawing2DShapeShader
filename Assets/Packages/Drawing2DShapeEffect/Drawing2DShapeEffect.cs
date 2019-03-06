a98989155f1688dfbde53144e8be691f1d95a5d5
XJINE <xjine.andorelseif@gmail.com>
2019-03-06 18:40:31 +0900

    Organize resources & Update sample.

--- Assets/Packages/Drawing2DShapeEffect/Drawing2DShapeEffect.cs
+++ Assets/Packages/Drawing2DShapeEffect/Drawing2DShapeEffect.cs
@@ -7,11 +7,12 @@ public class Drawing2DShapeEffect : ImageEffectBase
 {
     #region Enum
 
-    public enum ShapeType : int
+    public enum Shape : int
     {
         Circle = 0,
         Ring   = 1,
-        Sqare  = 2
+        Sqare  = 2,
+        Rect   = 3,
     }
 
     #endregion Enum
@@ -19,9 +20,9 @@ public class Drawing2DShapeEffect : ImageEffectBase
     #region Struct
 
     [System.Serializable]
-    public struct Shape
+    public struct ShapeData
     {
-        public int     type;
+        public Shape   shape;
         public Vector4 position;
         public Vector4 parameter;
         public Color   color;
@@ -31,14 +32,19 @@ public class Drawing2DShapeEffect : ImageEffectBase
 
     #region Field
 
-    private ComputeBuffer shapeBuffer;
-    public  List<Shape>   shapes;
+    private ComputeBuffer   shapeBuffer;
+    public  List<ShapeData> shapes;
 
     private new Camera camera;
 
     #endregion Field
 
     #region Property
+
+    //public IReadOnlyList<ShapeData> Shapes
+    //{
+    
+    //}
 
     #endregion Property
 
@@ -54,7 +60,7 @@ public class Drawing2DShapeEffect : ImageEffectBase
     {
         if (this.shapes.Count != 0)
         {
-            this.shapeBuffer = new ComputeBuffer(this.shapes.Count, Marshal.SizeOf(typeof(Shape)));
+            this.shapeBuffer = new ComputeBuffer(this.shapes.Count, Marshal.SizeOf(typeof(ShapeData)));
             this.shapeBuffer.SetData(this.shapes);
         }
 
@@ -74,68 +80,83 @@ public class Drawing2DShapeEffect : ImageEffectBase
 
     public void DrawCircle(Vector3 posInWorld, Color color, float radius, int index = -1)
     {
-        DrawCircle(this.camera.WorldToScreenPoint(posInWorld), color, radius, index);
+        DrawCircle((Vector2)this.camera.WorldToViewportPoint(posInWorld), color, radius, index);
     }
 
     public void DrawCircle(Vector2 posInScreen, Color color, float radius, int index = -1)
     {
-        Shape shape = new Shape()
+        ShapeData shape = new ShapeData()
         {
-            type      = 0,
+            shape      = Shape.Circle,
             position  = new Vector4(posInScreen.x, posInScreen.y, 0, 0),
             parameter = new Vector4(radius, 0, 0, 0),
             color     = color
         };
 
-        AddShape(shape);
+        DrawShape(shape);
     }
 
     public void DrawRing(Vector3 posInWorld, Color color, float innerRadius, float outerRadius, int index = -1)
     {
-        DrawRing(this.camera.ViewportToWorldPoint(posInWorld), color, innerRadius, outerRadius, index);
+        DrawRing((Vector2)this.camera.WorldToViewportPoint(posInWorld), color, innerRadius, outerRadius, index);
     }
 
     public void DrawRing(Vector2 posInScreen, Color color, float innerRadius, float outerRadius, int index = -1)
     {
-        Shape shape = new Shape()
+        ShapeData shape = new ShapeData()
         {
-            type      = 1,
+            shape      = Shape.Ring,
             position  = new Vector4(posInScreen.x, posInScreen.y, 0, 0),
             parameter = new Vector4(innerRadius, outerRadius, 0, 0),
             color = color
         };
 
-        AddShape(shape);
+        DrawShape(shape);
     }
 
     public void DrawSqare(Vector3 posInWorld, Color color, float size, int index = -1)
     {
-        DrawSqare(this.camera.ViewportToWorldPoint(posInWorld), color, size, index);
+        DrawSqare((Vector2)this.camera.WorldToViewportPoint(posInWorld), color, size, index);
     }
 
     public void DrawSqare(Vector2 posInScreen, Color color, float size, int index = -1)
     {
-        Shape shape = new Shape()
+        ShapeData shape = new ShapeData()
         {
-            type      = 2,
+            shape      = Shape.Sqare,
             position  = new Vector4(posInScreen.x, posInScreen.y, 0, 0),
             parameter = new Vector4(size, 0, 0, 0),
             color     = color
         };
 
-        AddShape(shape);
+        DrawShape(shape);
     }
 
-    private void AddShape(Shape shape, int index = -1)
+    public void DrawRect(Vector3 posInWorld1, Vector3 posInWorld2, Color color, int index = -1)
     {
-        if (index < 0)
+        DrawRect((Vector2)this.camera.WorldToViewportPoint(posInWorld1),
+                 (Vector2)this.camera.WorldToViewportPoint(posInWorld2),
+                 color,
+                 index);
+    }
+
+    public void DrawRect(Vector2 posInScreen1, Vector2 posInScreen2, Color color, int index = -1)
+    {
+        ShapeData shape = new ShapeData()
         {
-            this.shapes.Add(shape);
-        }
-        else
-        {
-            this.shapes.Insert(index, shape);
-        }
+            shape     = Shape.Rect,
+            position  = new Vector4(posInScreen1.x, posInScreen1.y, posInScreen2.x, posInScreen2.y),
+            parameter = new Vector4(0, 0, 0, 0),
+            color     = color
+        };
+
+        DrawShape(shape);
+    }
+
+    public void DrawShape(ShapeData shape, int index = -1)
+    {
+        index = Mathf.Max(0, Mathf.Min(shapes.Count, index));
+        this.shapes.Insert(index, shape);
     }
 
     #endregion Method
---
+++ Assets/Sample.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Sample : MonoBehaviour
+{
+    public Drawing2DShapeEffect effect;
+
+    void Update()
+    {
+        this.effect.shapes.Clear();
+
+        this.effect.DrawCircle(new Vector2(0, 0), Color.red, 1);
+
+        this.effect.DrawRing  (new Vector2(0.8f, 0.8f), Color.green, 0.1f, 0.15f);
+
+        this.effect.DrawSqare (this.transform.position, Color.blue, 0.2f);
+
+        this.effect.DrawRect(new Vector2(0.5f, 0), new Vector2(1, 0.5f), new Color(1, 1, 0, 0.5f));
+    }
+}
